5289867d4a2cc9a8503615b055082ce37aa8547e
Evgenii-Kruteniuk <muster00100@yandex.ru>
2024-05-28 17:11:52 +0300

    28.05.24 Рандомайзер и рандомная смена фона

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-/*Список в котором можно кликом выделять выбранную книгу.*/
+/*Рандомный счетчик и рандомная смена цвета фона*/
 
-// const ol = document.querySelector("ol");
-// const allLi = document.querySelectorAll("li");
+const randomCount = document.querySelector(".random");
+const button = document.querySelector(".button");
 
-// ol.addEventListener("click", (e) => {
-//   if (e.target.closest("li")) {
-//     allLi.forEach((el) => el.classList.remove("colorLi"));
-//     e.target.classList.add("colorLi");
-//   }
-// });
+button.addEventListener("click", random);
 
-/*Создадим светофор, который по нажатию на кнопку старт будут менять цвет*/
-
-const buttonTrafficLights = document.querySelector(".buttonTrafficLights");
-const allCircle = document.querySelectorAll(".circle");
-
-buttonTrafficLights.addEventListener("click", redColor);
-
-function redColor() {
-  allCircle[2].classList.remove("green");
-  allCircle[0].classList.add("red");
-  buttonTrafficLights.removeEventListener("click", redColor);
-  buttonTrafficLights.addEventListener("click", yellowColor);
+function random() {
+  randomCount.innerText = `${Math.floor(Math.random() * 100)}`;
+  randomCount.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},
+     ${Math.floor(Math.random() * 255)}, 
+  ${Math.floor(Math.random() * 255)})`;
 }
 
-function yellowColor() {
-  allCircle[0].classList.remove("red");
-  allCircle[1].classList.add("yellow");
-  buttonTrafficLights.removeEventListener("click", yellowColor);
-  buttonTrafficLights.addEventListener("click", greenColor);
-}
-
-function greenColor() {
-  allCircle[1].classList.remove("yellow");
-  allCircle[2].classList.add("green");
-  buttonTrafficLights.removeEventListener("click", greenColor);
-  buttonTrafficLights.addEventListener("click", redColor);
-}
-
-/*Автоматическое моргание светофора с интервалос 1 сек*/
-
-// setInterval((outoClicker) => buttonTrafficLights.click(), 1000);
-
-//Тут произошли новые изменения
+setInterval(() => button.click(), 1000);
