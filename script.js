65a0022cc331f75885c9cbd3253b7935b6ed3826
Evgenii-Kruteniuk <muster00100@yandex.ru>
2024-05-31 13:07:35 +0300

    31.05.24

--- script.js
+++ script.js
@@ -1,52 +1,8 @@
-/*Рандомный счетчик и рандомная смена цвета фона*/
+// const formArr = document.querySelector(".form");
+// Array.from(formArr.elements).forEach((el) => console.log(el));
 
-// const randomCount = document.querySelector(".random");
-// const button = document.querySelector(".button");
+const forms = document.forms;
 
-// button.addEventListener("click", random);
-
-// function random() {
-//   randomCount.innerText = `${Math.floor(Math.random() * 100)}`;
-//   randomCount.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},
-//      ${Math.floor(Math.random() * 255)},
-//   ${Math.floor(Math.random() * 255)})`;
-// }
-
-// setInterval(() => button.click(), 1000);
-
-const button = document.querySelector(".button");
-const random1 = document.querySelector(".random1");
-const random2 = document.querySelector(".random2");
-const allLi = document.querySelectorAll("li");
-const ol = document.querySelector("ol");
-const arr = [
-  "Это цитата дня!",
-  "Это самая лучшая цитата, которую я слышал",
-  "Вау, никогда не забуду",
-  "Это что-то новенькое",
-  "Впервые слышу",
-  "Мне об этом друг рассказывал",
-  "Вау!!!!",
-  "Полная фигня",
-  "Это нужно записать",
-  " Хрен такое запомнишь)",
-];
-
-button.addEventListener("click", random);
-
-function random() {
-  allLi.forEach((el) => el.classList.remove("red"));
-  random1.innerText = `${arr[Math.floor(Math.random() * 10)]}`;
-
-  let param = Math.floor(Math.random() * 10);
-  random2.innerText = `${allLi[param].innerText}`;
-  allLi[param].classList.add("red");
-}
-
-ol.addEventListener("click", (e) => {
-  if (e.target.closest("li")) {
-    allLi.forEach((el) => el.classList.remove("red"));
-    random2.innerText = e.target.innerText;
-    e.target.classList.add("red");
-  }
-});
+const arr =Array.from(forms[0].elements)
+  .filter((el) => el.type === "radio")
+  .forEach((el) => console.log(el));
