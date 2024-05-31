cfe9854e81997eacd476d6e9fc952beb277628bc
Evgenii-Kruteniuk <muster00100@yandex.ru>
2024-05-31 16:53:39 +0300

    31.05.24

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// const formArr = document.querySelector(".form");
-// Array.from(formArr.elements).forEach((el) => console.log(el));
-
 const forms = document.forms;
+const content = document.getElementById("content");
+const buttonNode = document.getElementById("sendForm");
 
-const arr =Array.from(forms[0].elements)
-  .filter((el) => el.type === "radio")
-  .forEach((el) => console.log(el));
+buttonNode.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  Array.from(forms[0].elements).forEach((el) => {
+    if (
+      (el.type === "radio" && el.checked) ||
+      (el.type === "checkbox" && el.checked)
+    ) {
+      const p = document.createElement("p");
+      p.innerText = `Вы воспользовались радио/чекбоксом и выбрали: ${el.value}`;
+      content.append(p);
+    } else if (
+      el.name === "reviev" ||
+      el.type === "text" ||
+      el.type === "mail"
+    ) {
+      const p = document.createElement("p");
+      p.innerText = `Вы ввели в поле: ${el.value}`;
+      content.append(p);
+    } else if (el.name === "country") {
+      Array.from(el.children).forEach((elem) => {
+        if (elem.selected) {
+          const p = document.createElement("p");
+          p.innerText = `Вы выбрали из списка: ${elem.value}`;
+          content.append(p);
+        }
+      });
+    }
+  });
+});
